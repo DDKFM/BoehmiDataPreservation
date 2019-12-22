aa7c78e3db80a1c715875cd6795525c3dca18b77
DDKFM <maxschaedlich@hotmail.de>
2019-12-22 19:08:17 +0100

    change frontend to vue.js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,36 +1,89 @@
-$(document).ready(function(){
-    sendRequest(20, 0, true)
+var app = new Vue({
+    el: '#app',
+    data: {
+        searchQuery : '',
+        currentPage : 0,
+        pageCount : 0,
+        gifs : [],
+    },
+    methods : {
+        sendRequest : function (limit, page) {
+            this.currentPage = page
+            var query = app.searchQuery == '' ? '' : '&query=' + app.searchQuery
+            var offset = page * limit
+            $.get('/v1/search?limit=' + limit + "&offset=" + offset + query, function(response) {
+                app.gifs = response.gifs
+                app.pageCount = Math.ceil(response.count / response.limit)
+                $('.chips-placeholder').chips({
+                    placeholder: 'Tag hinzufügen',
+                    secondaryPlaceholder: '+Tag',
+                    onChipAdd : function() {
+                        var chips = $('#' + this.el.id + " .chip").text().replace("<i class=\"material-icons close\">close</i>", "")
+                        console.log(chips.split(" "))
+                        console.log(this)
+                        console.log(this.el.id)
+                    }
+                });
+            })
+        },
+        sendKeywords : function(tweetId, keywords) {
+
+        },
+        getImageData : function(url) {
+            return url + "/data"
+        },
+        getImageFilename : function(url) {
+            return url.replace('/v1/gifs/', '') + ".mp4"
+        },
+        getTweetId : function(url) {
+            return url.replace('/v1/gifs/', '')
+        },
+        nextPage : function() {
+            return app.currentPage + 1 % app.pageCount
+        },
+        prevPage : function() {
+            return app.currentPage == 0 ? 0 : app.currentPage - 1;
+        },
+        onEnter : function(gif) {
+            var tweetId = app.getTweetId(gif.url)
+            console.log(tweetId)
+            var newChip = $('#' + tweetId).val()
+            console.log(newChip)
+            gif.keywords.push(newChip)
+            $.ajax({
+                type: "POST",
+                url: "/v1/gifs/" + tweetId + "/keywords",
+                data: JSON.stringify(gif.keywords),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data){console.log(data);},
+                failure: function(errMsg) {
+                    console.log(errorMsg)
+                }
+            });
+            $('#' + tweetId).val("")
+        },
+        onDelete : function(gif, keyword) {
+            var tweetId = app.getTweetId(gif.url)
+            console.log(tweetId)
+            var newChip = $('#' + tweetId).val()
+            console.log(newChip)
+            gif.keywords.splice( gif.keywords.indexOf(keyword), 1 );
+            $.ajax({
+                type: "POST",
+                url: "/v1/gifs/" + tweetId + "/keywords",
+                data: JSON.stringify(gif.keywords),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data){console.log(data);},
+                failure: function(errMsg) {
+                    console.log(errorMsg)
+                }
+            });
+            $('#' + tweetId).val("")
+        }
+    }
 });
 
-
-
-function sendRequest(limit, offset, newRequest) {
-    var query = $('#query').val()
-    if(newRequest) {
-        $('#paging').html("")
-    }
-    console.log(query)
-    $.get('/v1/search?limit=' + limit + "&offset=" + (offset * limit) + (query == "" ? "" : "&query=" + query), function(data) {
-        $('#gifs').html("")
-        var gifs = data.gifs
-        var pages = data.count / data.limit
-        if(newRequest) {
-            for(var page = 0; page <= pages ; page++) {
-                var active = offset == page ? "active" : "waves-effect"
-                if(newRequest)
-                    $('#paging').append("<li class='page " + active + "' id='page_" + page + "'><a href='javascript:sendRequest(20, " + page + ", false)'>" + page + "</a></li>")
-            }
-        }
-        gifs.forEach(function(item, index) {
-            $('#gifs').append("<div class=\"card\">\n" +
-                "        <div class=\"card-content\">" +
-                "           <video autoplay controls loop playsinline=\"\" style='width: 100%;max-width: 500px' preload='auto' src=\"" + item.url + "/data\" type=\"video/mp4\"></video><br/>" +
-                "        </div>\n" +
-                "        <div class=\"card-action\">\n" +
-                "          <a href='" + item.url + "/data' download='" + item.url.replace("/v1/gifs/", "")  + ".mp4'><i class='fa fa-download'></i></a>" +
-                "           <a href='" + item.tweetUrl + "' title='Link zum Tweet' target=\"_blank\"><i class='fa fa-twitter'></i></a>" +
-                "        </div>\n" +
-                "      </div>")
-        })
-    })
-}
+app.searchQuery = ""
+app.sendRequest(20, 0)
