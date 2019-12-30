7c6b3e2ece666e3984afdf4e8a7ddd1f901cd327
DDKFM <maxschaedlich@hotmail.de>
2019-12-30 21:10:20 +0100

    cache all tweets every 15min

--- src/main/kotlin/de/ddkfm/DataInitializer.kt
+++ src/main/kotlin/de/ddkfm/DataInitializer.kt
@@ -8,15 +8,28 @@ import org.apache.commons.codec.digest.DigestUtils
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import twitter4j.Paging
+import java.util.concurrent.*
+import kotlin.concurrent.thread
 
 @Component
 class DataInitializer : CommandLineRunner {
     override fun run(vararg args: String) {
+        val scheduler = Executors.newScheduledThreadPool(1)
+        val fetchingInterval = System.getenv("FETCHING_INTERVAL")?.toLongOrNull() ?: 15L
+        scheduler.scheduleAtFixedRate({ scanTwitterUsers() }, 0, fetchingInterval, TimeUnit.MINUTES)
+    }
+
+    fun scanTwitterUsers() {
+        println("cache Twitter Users")
         val users = System.getenv("TWITTER_USERS")?.split(",") ?: emptyList()
         for(user in users) {
-            Twitter.doWithTwitter { indexTweetsFromUser(user) }
+            thread {
+                println("Cache user $user")
+                Twitter.doWithTwitter { indexTweetsFromUser(user) }
+            }
         }
     }
+
 
     fun twitter4j.Twitter.indexTweetsFromUser(user : String) {
         var page = 1
@@ -30,42 +43,44 @@ class DataInitializer : CommandLineRunner {
                     .filter { tweet -> tweet.mediaEntities.isNotEmpty() }//leere Entities raus
                     .filter { tweet -> tweet.mediaEntities.any { it.type == "animated_gif" } }//nur gifs bitte
                     .toList()
-                println("Tweets fetched: ${tweets.size}")
+                thread {
+                    println("Tweets fetched: ${tweets.size}")
 
-                for(tweet in tweets) {
-                    val existing = LuceneRepository.searchForAnyId(tweet.id)
-                    LuceneRepository.urlCache.get(tweet.id) { tweet.mediaEntities.map { it.expandedURL }.first() }
-                    LuceneRepository.posterCache.get(tweet.id) {tweet.mediaEntities.map { it.mediaURLHttps }.first()}
-                    if(existing != null)
-                        continue//if the tweetId is already in the index, continue
-                    val tweetUrl = tweet.mediaEntities
-                        .map { it.videoVariants.map { it.url } }
-                        .flatten()
-                        .first()//fetch the mediaUrl
-                    val bytes = tweetUrl?.download()//Download the GIF
-                        ?: continue
-                    val hash = DigestUtils.sha256Hex(bytes)
-                    val mainDocument = LuceneRepository.searchForHash(hash)
-                    if(mainDocument != null) {//any tweet with the same gif hash
-                        LuceneRepository.update(mainDocument["twitterId"].toLong()) {  ->
-                            appendOrCreate("text", tweet.text)
-                            appendOrCreate("sameTweetIds", tweet.id)
+                    for(tweet in tweets) {
+                        val existing = LuceneRepository.searchForAnyId(tweet.id)
+                        LuceneRepository.urlCache.get(tweet.id) { tweet.mediaEntities.map { it.expandedURL }.first() }
+                        LuceneRepository.posterCache.get(tweet.id) {tweet.mediaEntities.map { it.mediaURLHttps }.first()}
+                        if(existing != null)
+                            continue//if the tweetId is already in the index, continue
+                        val tweetUrl = tweet.mediaEntities
+                            .map { it.videoVariants.map { it.url } }
+                            .flatten()
+                            .first()//fetch the mediaUrl
+                        val bytes = tweetUrl?.download()//Download the GIF
+                            ?: continue
+                        val hash = DigestUtils.sha256Hex(bytes)
+                        val mainDocument = LuceneRepository.searchForHash(hash)
+                        if(mainDocument != null) {//any tweet with the same gif hash
+                            LuceneRepository.update(mainDocument["twitterId"].toLong()) {  ->
+                                appendOrCreate("text", tweet.text)
+                                appendOrCreate("sameTweetIds", tweet.id)
+                            }
+                            println("duplicate GIF for tweetId ${mainDocument["twitterId"].toLong()} found")
+                            continue
                         }
-                        println("duplicate GIF for tweetId ${mainDocument["twitterId"].toLong()} found")
-                        continue
+                        GifRepository.storeGif(tweet.id, bytes)
+                        LuceneRepository.create(tweet.id) {
+                            create("twitterId", tweet.id)
+                            create("tweet", tweet.text)
+                            create("hash", hash)
+                            create("user", tweet.user.screenName)
+                            create("twitterUrl", tweet.mediaEntities.map { it.expandedURL }.first())
+                            create("keywords", "neomagazin")
+                            create("sameTweetIds", "")
+                            create("deleted", false)
+                        }
+                        println("tweet with id ${tweet.id} created (hash: $hash)")
                     }
-                    GifRepository.storeGif(tweet.id, bytes)
-                    LuceneRepository.create(tweet.id) {
-                        create("twitterId", tweet.id)
-                        create("tweet", tweet.text)
-                        create("hash", hash)
-                        create("user", tweet.user.screenName)
-                        create("twitterUrl", tweet.mediaEntities.map { it.expandedURL }.first())
-                        create("keywords", "neomagazin")
-                        create("sameTweetIds", "")
-                        create("deleted", false)
-                    }
-                    println("tweet with id ${tweet.id} created (hash: $hash)")
                 }
                 if(tweets.isEmpty())
                     nullCounter++
--- src/main/kotlin/de/ddkfm/Twitter.kt
+++ src/main/kotlin/de/ddkfm/Twitter.kt
@@ -1,13 +1,35 @@
 package de.ddkfm
 
+import twitter4j.RateLimitStatusEvent
+import twitter4j.RateLimitStatusListener
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
+import java.util.concurrent.locks.ReentrantLock
 
 object Twitter {
     private val twitter = TwitterFactory.getSingleton()
+    val rateLimitLock = ReentrantLock()
+    val listener = twitter.addRateLimitStatusListener(object : RateLimitStatusListener {
+        override fun onRateLimitReached(p0: RateLimitStatusEvent?) {
+            println(p0)
+        }
+
+        override fun onRateLimitStatus(p0: RateLimitStatusEvent?) {
+            val remaining = p0?.rateLimitStatus?.remaining ?: return
+            println("rateLimit: $remaining")
+            if(remaining < 10) {
+                rateLimitLock.lock()
+            } else if(rateLimitLock.isLocked) {
+                rateLimitLock.unlock()
+            }
+        }
+
+    })
 
     fun <T> doWithTwitter(doThings : Twitter.() -> T) : T  {
+        while(rateLimitLock.isLocked)
+            Thread.sleep(500)
         return twitter.doThings()
     }
 }
