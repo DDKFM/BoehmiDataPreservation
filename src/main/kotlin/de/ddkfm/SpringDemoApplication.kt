52385b6fd760aab93089d64e90fa50027981f74a
DDKFM <maxschaedlich@hotmail.de>
2020-10-14 18:08:07 +0200

    some spring boot annotations

--- src/main/kotlin/de/ddkfm/SpringDemoApplication.kt
+++ src/main/kotlin/de/ddkfm/SpringDemoApplication.kt
@@ -2,10 +2,12 @@ package de.ddkfm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
-@Configuration
+@EnableTransactionManagement
+@EnableSpringDataWebSupport
 class SpringDemoApplication
 
 fun main(args: Array<String>) {
