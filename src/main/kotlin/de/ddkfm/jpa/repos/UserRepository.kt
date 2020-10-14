cbc66c6c26ee64b414de0e9d135c1a58d0920a96
DDKFM <maxschaedlich@hotmail.de>
2020-10-14 19:30:46 +0200

    remove cachable from user repository for now

--- src/main/kotlin/de/ddkfm/jpa/repos/UserRepository.kt
+++ src/main/kotlin/de/ddkfm/jpa/repos/UserRepository.kt
@@ -1,13 +1,11 @@
 package de.ddkfm.jpa.repos
 
 import de.ddkfm.jpa.models.Tweeter
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<Tweeter, String> {
-    @Cacheable("users")
     fun findByUserId(userId : Long) : Optional<Tweeter>
 }
