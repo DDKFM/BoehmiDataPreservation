e7d3059f511f67bea4ad9d497daa3a72397bf10a
DDKFM <maxschaedlich@hotmail.de>
2020-10-22 19:28:08 +0200

    configure the twitter connection by using the yaml configuration

--- src/main/kotlin/de/ddkfm/Twitter.kt
+++ src/main/kotlin/de/ddkfm/Twitter.kt
@@ -1,16 +1,24 @@
 package de.ddkfm
 
+import de.ddkfm.configuration.DataConfiguration
 import de.ddkfm.twitter.StreamListener
 import twitter4j.*
 import twitter4j.Twitter
+import twitter4j.conf.Configuration
+import twitter4j.conf.ConfigurationBuilder
 import java.util.concurrent.locks.ReentrantLock
 
 object Twitter {
-    private val twitter = TwitterFactory.getSingleton()
+    private val configuration = ConfigurationBuilder()
+        .setGZIPEnabled(true)
+        .setOAuthAccessToken(DataConfiguration.config.twitter.accessToken)
+        .setOAuthAccessTokenSecret(DataConfiguration.config.twitter.accessTokenSecret)
+        .setOAuthConsumerKey(DataConfiguration.config.twitter.consumerKey)
+        .setOAuthConsumerSecret(DataConfiguration.config.twitter.consumerKeySecret)
+        .build()
+    private val twitter by lazy {TwitterFactory(configuration).instance }
     val rateLimitLock = ReentrantLock()
-    private val streamConnection by lazy {
-        TwitterStreamFactory.getSingleton()
-    }
+    private val streamConnection by lazy { TwitterStreamFactory(configuration).instance }
 
      fun <T> doWithTwitter(doThings : Twitter.() -> T) : T  {
         while(rateLimitLock.isLocked)
