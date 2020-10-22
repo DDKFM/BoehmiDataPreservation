374cda0d760cefc1f99dc829ae07ad6ab7ae00bd
DDKFM <maxschaedlich@hotmail.de>
2020-10-22 19:27:37 +0200

    Scheduler to import new Gifs into the system

---
+++ src/main/kotlin/de/ddkfm/tasks/GifQueueScheduler.kt
@@ -0,0 +1,40 @@
+package de.ddkfm.tasks
+
+import de.ddkfm.Twitter
+import de.ddkfm.jpa.repos.GifQueueRepository
+import de.ddkfm.jpa.repos.GifRepository
+import de.ddkfm.jpa.repos.TweetRepository
+import de.ddkfm.jpa.repos.UserRepository
+import de.ddkfm.utils.StatusUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import javax.transaction.Transactional
+
+@Component
+class GifQueueScheduler {
+    @Autowired
+    lateinit var queueRepo : GifQueueRepository
+
+    @Autowired
+    lateinit var statusUtils : StatusUtils
+
+    @Scheduled(fixedRate = 30000)
+    @Transactional
+    fun run() {
+        val gifs = queueRepo.findByAcceptedTrue(PageRequest.of(0, 20, Sort.by("created").ascending()))
+        if(gifs.isEmpty())
+            return
+        for(gif in gifs) {
+            Twitter.doWithTwitter {
+                for(tweetId in gif.tweetIds) {
+                    val status = showStatus(tweetId)
+                    statusUtils.toTweet(status, keywords = gif.keywords)
+                }
+            }
+        }
+        queueRepo.deleteAll(gifs)
+    }
+}
