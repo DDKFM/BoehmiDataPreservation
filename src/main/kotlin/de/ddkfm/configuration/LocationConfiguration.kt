c968bc8068f8eccae996bfbfc40d5ffd63c40e74
DDKFM <maxschaedlich@hotmail.de>
2020-10-24 14:16:19 +0200

    add icons configuration

--- src/main/kotlin/de/ddkfm/configuration/LocationConfiguration.kt
+++ src/main/kotlin/de/ddkfm/configuration/LocationConfiguration.kt
@@ -3,5 +3,6 @@ package de.ddkfm.configuration
 data class LocationConfiguration(
     var lucene : String? = null,
     var gifs : String = "/data/gifs",
-    var videos : String = "/data/videos"
+    var videos : String = "/data/videos",
+    var icons: String = "classpath:static/icons"
 )
