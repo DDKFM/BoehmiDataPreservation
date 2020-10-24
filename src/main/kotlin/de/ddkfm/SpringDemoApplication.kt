5ea5cfec776d84db8bf51f34825614eace21a5ce
DDKFM <maxschaedlich@hotmail.de>
2020-10-24 13:35:30 +0200

    enable Auditing

--- src/main/kotlin/de/ddkfm/SpringDemoApplication.kt
+++ src/main/kotlin/de/ddkfm/SpringDemoApplication.kt
@@ -2,6 +2,7 @@ package de.ddkfm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 @EnableTransactionManagement
 @EnableSpringDataWebSupport
 @EnableScheduling
+@EnableJpaAuditing
 class SpringDemoApplication
 
 fun main(args: Array<String>) {
