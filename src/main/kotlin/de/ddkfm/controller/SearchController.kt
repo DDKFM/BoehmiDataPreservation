5df00a3c82299e0e0c1c55b46b2cddb21ddc0733
DDKFM <maxschaedlich@hotmail.de>
2020-11-01 19:34:32 +0100

    move search function to native query

--- src/main/kotlin/de/ddkfm/controller/SearchController.kt
+++ src/main/kotlin/de/ddkfm/controller/SearchController.kt
@@ -5,6 +5,7 @@ import de.ddkfm.jpa.repos.TweetRepository
 import de.ddkfm.models.GifResponse
 import de.ddkfm.repositories.LuceneRepository
 import de.ddkfm.models.GifSearchResponse
+import de.ddkfm.tracking.TrackExecutionTime
 import de.ddkfm.utils.toGifResponse
 import org.apache.lucene.document.Document
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 
 @RestController
@@ -25,23 +27,49 @@ class SearchController {
     @Autowired
     lateinit var tweetRepo : TweetRepository
 
-    @GetMapping("/search")
     fun search(
-        @RequestParam("query", defaultValue = "") query: String = "",
-        @RequestParam("limit", defaultValue = "50") limit : Int = 50,
-        @RequestParam("offset", defaultValue = "0") offset : Int = 0
+        query: String = "",
+        limit : Int = 50,
+        offset : Int = 0
     ): ResponseEntity<GifSearchResponse> {
         val pageRequest = PageRequest.of(offset / limit, limit)
         val gifs = if(query.isEmpty())
             gifRepo.findByDeletedFalse(pageRequest)
         else
             tweetRepo.findByKeywordsContains(query.toLowerCase(), pageRequest)
+        gifs.content.distinctBy { it.id }.map { it.toGifResponse() }
         return ok(
             GifSearchResponse(
                 count = gifs.totalElements,
                 limit = limit,
                 offset = offset,
                 gifs = gifs.content.distinctBy { it.id }.map { it.toGifResponse() }
+            )
+        )
+    }
+
+    @TrackExecutionTime
+    @GetMapping("/search")
+    fun searchExperimental(
+        @RequestParam("query", defaultValue = "") query: String = "",
+        @RequestParam("limit", defaultValue = "50") limit : Int = 50,
+        @RequestParam("offset", defaultValue = "0") offset : Int = 0
+    ): ResponseEntity<GifSearchResponse> {
+        if(query.isEmpty())
+            return search(query, limit, offset)
+        val pageRequest = PageRequest.of(offset / limit, limit)
+        val gifs = gifRepo.blub(query, pageRequest)
+        return ok(
+            GifSearchResponse(
+                count = gifs.totalElements,
+                limit = limit,
+                offset = offset,
+                gifs = gifs.content.map { gif -> GifResponse(
+                    url = gif[0]!!.toString(),
+                    posterUrl = gif[1]!!.toString(),
+                    tweetUrls = gif[2]?.toString()?.split(",") ?: emptyList(),
+                    keywords = gif[3]?.toString()?.split(",") ?: emptyList()
+                ) }
             )
         )
     }
